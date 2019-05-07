bab3647d4d16d6f4fbbbecc04ccd7112ccfca094
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-05-07 20:32:08 +0530

    Layout changes homeview

--- src/components/Home/Home.view.js
+++ src/components/Home/Home.view.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 
 import { Intro } from './components/IntroMessage';
 import { TopBar } from './components/TopBar';
+import { AboutMeView } from './components/AboutMe';
 
 export default class DemoApp extends Component {
   render() {
     return (
       <div className="rootContainer">
-        <div className="parallaxLinearGradient p-3">
+        <div className="parallaxLinearGradient p-3 flex flex-col">
           <TopBar />
           <Intro
             message="Hello there!!"
             name=""
           />
+        </div>
+        <div className="flex flex-row p-4">
+          <AboutMeView />
         </div>
       </div>
     );
