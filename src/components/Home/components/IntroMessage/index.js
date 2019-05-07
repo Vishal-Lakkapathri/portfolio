7e645c4a4c6eac08e9d528cdda583ac376642015
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-05-07 20:32:41 +0530

    Intro and Top bar layout changes

--- src/components/Home/components/IntroMessage/index.js
+++ src/components/Home/components/IntroMessage/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export const Intro = ({ message, name }: string) => (
-  <h1
-    className="text-white font-mono text-center pt-32"
-  >
-    {`${message}
-    ${name}`}
-  </h1>
+  <div className="flex h-full justify-center items-center">
+    <h1
+      className="text-white font-mono"
+    >
+      {`${message}
+      ${name}`}
+    </h1>
+  </div>
 );
--- src/components/Home/components/TopBar/index.js
+++ src/components/Home/components/TopBar/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Tabs = ({ title }: string) => <h5 className="text-white fontGillSans">{title}</h5>;
 
 export const TopBar = () => (
-  <div className="displayFlex px-2 stickyHeader">
+  <div className="flex px-2 stickyHeader">
     <div className="w-2/3"><h2 className="text-white font-light">Vishal Lakkapathri</h2></div>
     <div className="displayFlex w-1/4 flex-item pt-3">
       <Tabs title="Home" />
