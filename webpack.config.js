6176e94f3c455e10ac8dead94a0e908aba174390
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-05-09 11:56:56 +0530

    Babel and Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react'],
+          presets: ['babel-preset-es2015', 'es2015', 'react', 'stage-2'],
         },
       },
       {
