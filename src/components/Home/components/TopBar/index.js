6263a12a27a767fb9f859ae417e3004ab178d14a
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-05-09 12:14:55 +0530

    Added Download resume

--- src/components/Home/components/TopBar/index.js
+++ src/components/Home/components/TopBar/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 
-const Tabs = ({ title }: string) => <h5 className="text-white fontGillSans">{title}</h5>;
+const vishalResume = '../../../../../assets/files/vishal-lakkapathri-resume.pdf';
 
-export const TopBar = () => (
+const Tabs = ({ title }: { title: string }) => (
+  <button
+    type="button"
+    className="text-white"
+  >
+    {title}
+  </button>
+);
+
+const DownloadResume = ({ title, onButtonPress }: { title: string, onButtonPress: Function}) => (
+  <a
+    href={vishalResume}
+    download
+  >
+    <button
+      type="button"
+      onClick={onButtonPress}
+      className="text-white"
+    >
+      {title}
+    </button>
+  </a>
+);
+
+export const TopBar = ({ onDownloadResume }: Function) => (
   <div className="flex px-2 stickyHeader items-center">
     <div className="w-2/3"><h2 className="text-white font-light">Vishal Lakkapathri</h2></div>
     <div className="displayFlex w-1/4 flex-item">
       <Tabs title="Home" />
       <Tabs title="About" />
-      <Tabs title="Resume" />
       <Tabs title="Contact" />
+      <DownloadResume
+        title="Download Resume"
+        onButtonPress={onDownloadResume}
+      />
     </div>
   </div>
 );
