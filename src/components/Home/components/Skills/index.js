0236774f70812bdca5b06ebb475249750b365b52
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-05-10 14:56:44 +0530

    Skills list

--- src/components/Home/components/Skills/index.js
+++ src/components/Home/components/Skills/index.js
@@ -1,37 +1,66 @@
 import React from 'react';
+import { FaReact, FaJsSquare, FaRetweet } from 'react-icons/fa';
+import { GiRam } from 'react-icons/gi';
+import { DiReact, DiCode } from 'react-icons/di';
 
-const SkillCard = ({ title, content }: string) => (
-  <div className="cardBackground items-center justify-center p-3">
-    <p className="text-xl font-semibold">{title}</p>
-    <div className="border border-red" />
-    <p className="text-sm">{content}</p>
-  </div>
-);
+// import JsLogo from '../../../../../assets/logos/js.png';
+// import ReactLogo from '../../../../../assets/logos/react.png';
+// import ReactNativeLogo from '../../../../../assets/logos/react-native.png';
+// import ReaduxLogo from '../../../../../assets/logos/redux.png';
+// import ES6Logo from '../../../../../assets/logos/es6.png';
 
-const Category = ({ categoryName }: string) => <h3 className="fontColor">{categoryName}</h3>;
+// const SkillCard = ({ title, content }: string) => (
+//   <div className="cardBackground items-center justify-center p-3">
+//     <p className="text-xl font-semibold">{title}</p>
+//     <div className="border border-red" />
+//     <p className="text-sm">{content}</p>
+//   </div>
+// );
+
+const Category = ({ categoryName }: string) => <h3 className="fontColor py-4">{categoryName}</h3>;
 
 export const SkillsList = () => (
-  <div className="flex flex-col items-center">
+  <div className="flex flex-col w-full px-5 items-center">
     <Category
       categoryName="Skills"
     />
-    <div className="w-full flex flex-row white px-6 justify-between pt-6">
-      <SkillCard
-        title="React Native"
-        content="React Native is an open-source mobile application framework created by Facebook. It is used to develop applications for Android, iOS and UWP by enabling developers to use React along with native platform"
-      />
-      <SkillCard
-        title="React"
-        content="React Native is an open-source mobile application framework created by Facebook. It is used to develop applications for Android, iOS and UWP by enabling developers to use React along with native platform"
-      />
-      <SkillCard
-        title="Redux"
-        content="React Native is an open-source mobile application framework created by Facebook. It is used to develop applications for Android, iOS and UWP by enabling developers to use React along with native platform"
-      />
-      <SkillCard
-        title="Ramda JS"
-        content="React Native is an open-source mobile application framework created by Facebook. It is used to develop applications for Android, iOS and UWP by enabling developers to use React along with native platform"
-      />
+    <div className="flex flex-row">
+      <div className="flex-col flex items-center justify-center">
+        <FaReact
+          style={{ color: '#61dafb', fontSize: 40 }}
+        />
+        <h3 className="text-center">React</h3>
+      </div>
+      <div className="flex-col flex items-center justify-center  ml-10">
+        <FaRetweet
+          style={{ color: '#ad6cbe', fontSize: 40 }}
+        />
+        <h3 className="text-center">Redux</h3>
+      </div>
+      <div className="flex-col flex items-center justify-center  ml-10">
+        <DiReact
+          style={{ color: '#333333', fontSize: 40 }}
+        />
+        <h3 className="text-center">React Native</h3>
+      </div>
+      <div className="flex-col flex items-center justify-center ml-10">
+        <FaJsSquare
+          style={{ color: '#f7df1e', fontSize: 40 }}
+        />
+        <h3 className="text-center">Java Script</h3>
+      </div>
+      <div className="flex-col flex items-center justify-center  ml-10">
+        <GiRam
+          style={{ color: '#ad6cbe', fontSize: 40 }}
+        />
+        <h3 className="text-center">Ramda</h3>
+      </div>
+      <div className="flex-col flex items-center justify-center  ml-10">
+        <DiCode
+          style={{ color: '#000000', fontSize: 40 }}
+        />
+        <h3 className="text-center">ES6</h3>
+      </div>
     </div>
   </div>
 );
