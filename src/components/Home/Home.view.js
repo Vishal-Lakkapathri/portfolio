b71524bb9902637313a5e03e7403a735a4834eec
Vishal Lakkapathri <vishal.lakkapathri@copart.com>
2019-05-11 19:38:09 +0530

    Navigate to div

--- src/components/Home/Home.view.js
+++ src/components/Home/Home.view.js
@@ -7,20 +7,37 @@ import { SkillsList } from './components/Skills';
 import StudyTimeLine from './components/StudyTimeLine';
 
 export default class DemoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.focusAboutMe = React.createRef();
+    this.focusMySkills = React.createRef();
+  }
+
+  onNavigate = (divName: string) => {
+    if (this[divName].current) {
+      this[divName].current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="parallaxLinearGradient p-3 flex flex-col">
-          <TopBar />
+          <TopBar
+            navigateTo={this.onNavigate}
+          />
           <Intro
             message="Hello there!! Welcome"
             name="I`m a front End Developer"
           />
         </div>
-        <div className="py-10 px-4">
+        <div className="py-10 px-4" ref={this.focusAboutMe}>
           <AboutMeView />
         </div>
-        <div className="pb-24 pt-4">
+        <div className="pb-24 pt-4" ref={this.focusMySkills}>
           <StudyTimeLine />
         </div>
         <div className="py-10">
--- src/components/Home/components/TopBar/index.js
+++ src/components/Home/components/TopBar/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const vishalResume = '../../../../../assets/files/vishal-lakkapathri-resume.pdf';
 
-const Tabs = ({ title }: { title: string }) => (
+const Tabs = ({ title, handleOnClick }: { title: string, handleOnClick: Function }) => (
   <button
     type="button"
-    className="text-white"
+    className="text-white pr-4"
+    onClick={handleOnClick}
   >
     {title}
   </button>
 );
 
-const DownloadResume = ({ title, onButtonPress }: { title: string, onButtonPress: Function}) => (
+const DownloadResume = ({ title, onButtonPress }: { title: string, onButtonPress: Function }) => (
   <a
     href={vishalResume}
     download
@@ -26,13 +27,22 @@ const DownloadResume = ({ title, onButtonPress }: { title: string, onButtonPress
   </a>
 );
 
-export const TopBar = ({ onDownloadResume }: Function) => (
-  <div className="flex px-2 stickyHeader items-center">
+export const TopBar = ({ onDownloadResume, navigateTo }: Function) => (
+  <div className="flex stickyHeader items-center">
     <div className="w-2/3"><h2 className="text-white font-light">Vishal Lakkapathri</h2></div>
-    <div className="displayFlex w-1/4 flex-item">
-      <Tabs title="Home" />
-      <Tabs title="About" />
-      <Tabs title="Contact" />
+    <div className="flex justify-end pl-24">
+      <Tabs
+        title="Skills"
+        handleOnClick={() => navigateTo('focusMySkills')}
+      />
+      <Tabs
+        title="Contact"
+        handleOnClick={() => navigateTo('focusAboutMe')}
+      />
+      <Tabs
+        title="About Me"
+        handleOnClick={() => navigateTo('focusAboutMe')}
+      />
       <DownloadResume
         title="Download Resume"
         onButtonPress={onDownloadResume}
